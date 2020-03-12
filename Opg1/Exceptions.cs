b1a6277b3a4b0150a58d95c6d1bf5eb845fdc521
LarsVegasAYY <50323739+LarsVegasAYY@users.noreply.github.com>
2020-03-12 01:13:30 +0100

    Add files via upload

---
+++ Opg1/Exceptions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Opg1
+{
+    public class ForfatterException : Exception
+    {
+        public ForfatterException(string msg) : base(msg) { }
+    }
+
+    public class SideAntalException : Exception
+    {
+        public SideAntalException(string msg) : base(msg) { }
+    }
+
+    public class Isbn13Exception : Exception
+    {
+        public Isbn13Exception(string msg) : base(msg) { }
+    }
+}
---
+++ REST/Controllers/BogController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Opg1;
+namespace REST.Controllers
+{
+    [Route("api/bog/")]
+    [ApiController]
+    public class BogController : ControllerBase
+    {
+
+        private static readonly List<Bog> books = new List<Bog>()
+        {
+            new Bog("Mit harem og mig!", "Lars den store", 222, "1234567890123"),
+            new Bog("Hvordan grapefrugt ændrede mit liv!", "Lars den store", 222, "1234567890124"),
+            new Bog("Wife mass exodus", "Lars den store", 222, "1234567890125"),
+            new Bog("NO THIS IS PATRICK", "Lars den store", 222, "1234567890126"),
+            new Bog("Hvis bare jeg var Tom", "Lars den store", 222, "1234567890127")
+        };
+        
+
+        // GET: api/Bog
+        [HttpGet]
+        public IEnumerable<Bog> Get()
+        {
+            return books;
+        }
+
+        // GET: api/Bog/5
+        [HttpGet("{isbn13}", Name = "Get")]
+        public Bog Get(string isbn13)
+        {
+            return books.Find(x=>x.Isbn13==isbn13);
+        }
+
+        // POST: api/Bog
+        [HttpPost]
+        public void Post([FromBody] Bog bog)
+        {
+            books.Add(bog);
+        }
+
+        // PUT: api/Bog/5
+        [HttpPut("{isbn13}")]
+        public void Put(string isbn13, [FromBody] Bog updateBog)
+        {
+            Bog bog = Get(isbn13);
+            if (bog != null)
+            {
+                bog.Titel = updateBog.Titel;
+                bog.Forfatter = updateBog.Forfatter;
+                bog.Sidetal = updateBog.Sidetal;
+            }
+
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{isbn13}")]
+        public void Delete(string isbn13)
+        {
+            books.Remove(Get(isbn13));
+        }
+    }
+}
